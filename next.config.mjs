e76b30b878b4a9959496b513fec7809457267e2d
vivekd17 <vivekvalvid17@gmail.com>
2025-04-03 21:52:01 +0530

    reset changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/Next.js-E-Commerce",
-  assetPrefix: "/Next.js-E-Commerce/",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "fakestoreapiserver.reactbd.com",
-      },
-    ],
-  },
-};
-
-export default nextConfig;
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "fakestoreapiserver.reactbd.com",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
